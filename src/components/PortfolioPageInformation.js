34ce8c1bae8af6524f3846df20bce1b30c5e5ff9
Keith <ozmaneku@gmail.com>
2020-06-17 14:11:06 -0400

    Created test divs within the portfolio page for my projects.

--- src/components/PortfolioPageInformation.js
+++ src/components/PortfolioPageInformation.js
@@ -4,7 +4,22 @@ class PortfolioPageInformation extends Component {
     render() {
         return (
             <div className='portfolioPageInformation' id='portfolioPageInformation'>
-                PORTFOLIO PAGE INFORMATION
+                <div className='testDiv'>
+                    <img src={require('./pictures/test-image.jpg')} className='testImage' alt='test' />
+                Test 1
+                </div>
+                <div className='testDiv'>
+                    <img src={require('./pictures/test-image.jpg')} className='testImage' alt='test' />
+                Test 2
+                </div>
+                <div className='testDiv'>
+                    <img src={require('./pictures/test-image.jpg')} className='testImage' alt='test' />
+                Test 3
+                </div>
+                <div className='testDiv'>
+                    <img src={require('./pictures/test-image.jpg')} className='testImage' alt='test' />
+                Test 4
+                </div>
             </div>
         );
     }
