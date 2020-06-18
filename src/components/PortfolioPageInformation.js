427fd0bb364b4dfae2ef625336002f97972991b2
Keith <ozmaneku@gmail.com>
2020-06-18 12:58:43 -0400

    Created mobile skeleton

--- src/components/PortfolioPageInformation.js
+++ src/components/PortfolioPageInformation.js
@@ -8,20 +8,20 @@ class PortfolioPageInformation extends Component {
                     Please scroll left to right.
                 </div>
 
-                <div className='testDiv'>
-                    <img src={require('./pictures/test-image.jpg')} className='testImage' alt='test' />
+                <div className='testDiv1'>
+                    <img src={require('./pictures/test-image.jpg')} className='testImage1' alt='test' />
                 Test 1
                 </div>
-                <div className='testDiv'>
-                    <img src={require('./pictures/test-image.jpg')} className='testImage' alt='test' />
+                <div className='testDiv2'>
+                    <img src={require('./pictures/test-image.jpg')} className='testImage2' alt='test' />
                 Test 2
                 </div>
-                <div className='testDiv'>
-                    <img src={require('./pictures/test-image.jpg')} className='testImage' alt='test' />
+                <div className='testDiv3'>
+                    <img src={require('./pictures/test-image.jpg')} className='testImage3' alt='test' />
                 Test 3
                 </div>
-                <div className='testDiv'>
-                    <img src={require('./pictures/test-image.jpg')} className='testImage' alt='test' />
+                <div className='testDiv4'>
+                    <img src={require('./pictures/test-image.jpg')} className='testImage4' alt='test' />
                 Test 4
                 </div>
             </div>
