71b5a39d91ae28f9aaf0020617f592351a836405
Keith <ozmaneku@gmail.com>
2020-06-25 09:45:06 -0400

    Beginning to add in portfolio divs and pictures.

--- src/components/PortfolioPageInformation.js
+++ src/components/PortfolioPageInformation.js
@@ -4,25 +4,22 @@ class PortfolioPageInformation extends Component {
     render() {
         return (
             <div className='portfolioPageInformation' id='portfolioPageInformation'>
-                <div className='pleaseScroll'>
+                <h2 className='pleaseScroll'>
                     Please scroll up and down.
+                </h2>
+
+                <div className='iAmManny'>
+                    <a href='https://upbeat-pare-9ca7a2.netlify.app/'>
+                        <img src={require('./pictures/IAmManny.jpg')} alt='IAmManny' className='iAmMannyPic'></img>
+                    </a>
+                    EXPLINATION HERE
                 </div>
 
-                <div className='testDiv1'>
-                    <img src={require('./pictures/test-image.jpg')} className='testImage1' alt='test' />
-                Test 1
-                </div>
-                <div className='testDiv2'>
-                    <img src={require('./pictures/test-image.jpg')} className='testImage2' alt='test' />
-                Test 2
-                </div>
-                <div className='testDiv3'>
-                    <img src={require('./pictures/test-image.jpg')} className='testImage3' alt='test' />
-                Test 3
-                </div>
-                <div className='testDiv4'>
-                    <img src={require('./pictures/test-image.jpg')} className='testImage4' alt='test' />
-                Test 4
+                <div className='FFXIV'>
+                    <a href='https://final-fantasy-project.herokuapp.com/'>
+                        <img src={require('./pictures/FFXIV.jpg')} alt="FFXIV" className='FFXIVPic'></img>
+                    </a>
+                    EXPLINATION HERE
                 </div>
             </div>
         );
