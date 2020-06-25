d4d118971ab7aeef4e249a0e7fe1b42bd8fed981
Keith <ozmaneku@gmail.com>
2020-06-25 10:41:23 -0400

    Refactored the portfolio page to use an object for less redundancy.

--- src/components/PortfolioPageInformation.js
+++ src/components/PortfolioPageInformation.js
@@ -2,48 +2,65 @@ import React, { Component } from 'react';
 
 class PortfolioPageInformation extends Component {
     render() {
+
+        const websiteObject = {
+            1: {
+                site: 'https://upbeat-pare-9ca7a2.netlify.app/',
+                img: './pictures/IAmManny.jpg',
+                alt: 'IAmManny',
+                text: 'EXPLINATION HERE: IAmManny'
+            },
+
+            2: {
+                site: 'https://final-fantasy-project.herokuapp.com/',
+                img: './pictures/FFXIV.jpg',
+                alt: 'FFXIV',
+                text: 'EXPLINATION HERE: FFXIV'
+            },
+
+            3: {
+                site: 'https://shielded-lake-76706.herokuapp.com/',
+                img: './pictures/AnimalCrossing.jpg',
+                alt: 'Animal Crossing',
+                text: 'EXPLINATION HERE: Animal Crossing'
+            },
+
+            4: {
+                site: 'https://character-data.herokuapp.com/',
+                img: './pictures/CharacterData.jpg',
+                alt: 'Character Data',
+                text: 'EXPLINATION HERE: Character Data'
+            },
+
+            5: {
+                site: 'https://nekublackjack.netlify.app/',
+                img: './pictures/BlackJack.jpg',
+                alt: 'Black Jack',
+                text: 'EXPLINATION HERE: Black Jack'
+            }
+        }
+
+
+
         return (
             <div className='portfolioPageInformation' id='portfolioPageInformation'>
                 <h2 className='pleaseScroll'>
                     Please scroll up and down.
                 </h2>
-
-                <div className='iAmManny'>
-                    <a href='https://upbeat-pare-9ca7a2.netlify.app/'>
-                        <img src={require('./pictures/IAmManny.jpg')} alt='IAmManny' className='iAmMannyPic'/>
-                    </a>
-                    EXPLINATION HERE
-                </div>
-
-                <div className='FFXIV'>
-                    <a href='https://final-fantasy-project.herokuapp.com/'>
-                        <img src={require('./pictures/FFXIV.jpg')} alt="FFXIV" className='FFXIVPic'/>
-                    </a>
-                    EXPLINATION HERE
-                </div>
-
-                <div className='animalCrossing'>
-                    <a href='https://shielded-lake-76706.herokuapp.com/'>
-                        <img src={require('./pictures/AnimalCrossing.jpg')} alt='Animal Crossing' className='animalCrossingPic'/>
-                    </a>
-                    EXPLINATION HERE
-                </div>
-
-                <div className='characterData'>
-                    <a href='https://character-data.herokuapp.com/'>
-                        <img src={require('./pictures/CharacterData.jpg')} alt='Character Data' className='characterDataPic'/>
-                    </a>
-                    EXPLINATION HERE
-                </div>
-
-                <div className='blackJack'>
-                    <a href='https://nekublackjack.netlify.app/'>
-                        <img src={require('./pictures/BlackJack.jpg')} alt='Black Jack' className='blackJackPic'/>
-                    </a>
-                    EXPLINATION HERE
-                </div>
-
+            <div>
+                {
+                    Object.keys(websiteObject).map((key) => {
+                        return <div>
+                            <a href={websiteObject[key].site}>
+                                <img src={require(`${websiteObject[key].img}`)} alt={websiteObject[key].alt} />
+                            </a>
+                            {websiteObject[key].text}
+                        </div>
+                    })
+                }
             </div>
+
+            </div >
         );
     }
 }
