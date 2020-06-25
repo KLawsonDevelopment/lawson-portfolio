73a663f93cd260a0a934c3c553f3fc20957e5cd6
Keith <ozmaneku@gmail.com>
2020-06-25 10:46:45 -0400

    Fixed react key issue.

--- src/components/PortfolioPageInformation.js
+++ src/components/PortfolioPageInformation.js
@@ -4,39 +4,44 @@ class PortfolioPageInformation extends Component {
     render() {
 
         const websiteObject = {
-            1: {
+            IAmManny: {
                 site: 'https://upbeat-pare-9ca7a2.netlify.app/',
                 img: './pictures/IAmManny.jpg',
                 alt: 'IAmManny',
-                text: 'EXPLINATION HERE: IAmManny'
+                text: 'EXPLINATION HERE: IAmManny',
+                key: 1
             },
 
-            2: {
+            FFXIV: {
                 site: 'https://final-fantasy-project.herokuapp.com/',
                 img: './pictures/FFXIV.jpg',
                 alt: 'FFXIV',
-                text: 'EXPLINATION HERE: FFXIV'
+                text: 'EXPLINATION HERE: FFXIV',
+                key: 2
             },
 
-            3: {
+            AnimalCrossing: {
                 site: 'https://shielded-lake-76706.herokuapp.com/',
                 img: './pictures/AnimalCrossing.jpg',
                 alt: 'Animal Crossing',
-                text: 'EXPLINATION HERE: Animal Crossing'
+                text: 'EXPLINATION HERE: Animal Crossing',
+                key: 3
             },
 
-            4: {
+            CharacterData: {
                 site: 'https://character-data.herokuapp.com/',
                 img: './pictures/CharacterData.jpg',
                 alt: 'Character Data',
-                text: 'EXPLINATION HERE: Character Data'
+                text: 'EXPLINATION HERE: Character Data',
+                key: 4
             },
 
-            5: {
+            BlackJack: {
                 site: 'https://nekublackjack.netlify.app/',
                 img: './pictures/BlackJack.jpg',
                 alt: 'Black Jack',
-                text: 'EXPLINATION HERE: Black Jack'
+                text: 'EXPLINATION HERE: Black Jack',
+                key: 5
             }
         }
 
@@ -50,7 +55,7 @@ class PortfolioPageInformation extends Component {
             <div>
                 {
                     Object.keys(websiteObject).map((key) => {
-                        return <div>
+                        return <div key={websiteObject[key].key}>
                             <a href={websiteObject[key].site}>
                                 <img src={require(`${websiteObject[key].img}`)} alt={websiteObject[key].alt} />
                             </a>
